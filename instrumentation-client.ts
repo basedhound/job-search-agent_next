5ce68decd94447a33fa6bab7880749229cf3fb75
fvukelic <franck.vukelic@gmail.com>
2026-06-06 19:47:28 +0700

    Cleanup

--- instrumentation-client.ts
+++
@@ -1,9 +0,0 @@
-import posthog from "posthog-js";
-
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
-  api_host: "/ingest",
-  ui_host: "https://eu.posthog.com",
-  capture_exceptions: true,
-  capture_dead_clicks: false,
-  debug: process.env.NODE_ENV === "development",
-});
